1566d681924827a4e0e7bf7fe7d836c1f6e21d35
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-05 04:24:16 +0000

    end of lesson 1

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -1,11 +1,15 @@
 function setup(){
     createCanvas(600, 400);
-    background(220);
+    background('black'); // 220
 }
 
 function draw(){
     fill(255, 0, 0);
-    ellipse(300,200,200,200);
-    
+    noStroke(); // no outline
+    ellipse(300,90,100,100);
+    fill('yellow');
+       ellipse(300,200,100,100);
+       fill('green') 
+           ellipse(300,310,100,100);
     // Todo: Challenge 3 Traffic Light
 }
