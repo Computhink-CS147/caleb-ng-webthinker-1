63b11ff4cca6e1c5da124fa13d081bb75f6f4311
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-19 03:46:55 +0000

    Jul 19, 2026, 3:46 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,9 +6,9 @@ function setup() {
   
 function draw() {
   circle(75,200,100)
-  circle(225,200,100)
-  circle(375,200,100)
-  circle(525,200,100)
+  circle(225,200,100);
+  circle(375,200,100);
+  circle(525,200,100);
   // Recap 1: Repeating Circles
 
   // Task 1: Colour Gradient
