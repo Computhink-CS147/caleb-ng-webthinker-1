e6531af96e8eb09bbfe4289fa50f78c79849deb3
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-26 04:10:35 +0000

    Jul 26, 2026, 4:10 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,11 +4,6 @@ function setup() {
     createCanvas(600, 800);
     background("aqua");
 }
-function setup() {
-    createCanvas(600, 400);
-    background(220);
-    // noLoop(); // Stops continuous drawing
-  }
   
 function draw() {
   circle(275,200,100);
