a82644b4dcc3d7a332319c17c76d0bff78af313b
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-26 04:08:50 +0000

    Jul 26, 2026, 4:08 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -7,12 +7,17 @@ function setup() {
 function setup() {
     createCanvas(600, 400);
     background(220);
-    noLoop(); // Stops continuous drawing
+    // noLoop(); // Stops continuous drawing
   }
   
 function draw() {
   circle(275,200,100);
 }
+Fill(shapecolor);
+circle(width/2, height/2, 200)
 function mousePressed() {
     shapecolor = color(random(0,255));
+}
+function mouseRelease() {
+    shapeColor="white"
 }
