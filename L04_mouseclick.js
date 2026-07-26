445e83add2438fc0e1048c5281d750471fb00976
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-26 03:35:54 +0000

    Jul 26, 2026, 3:35 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -13,7 +13,7 @@ function setup() {
 function draw() {
   circle(75,200,100);
   circle(225,200,100);
-  circle(375,200,375);
+  circle(375,200,100);
   circle(525,200,100);
   // Recap 1: Repeating Circles
 let circleDiameter= 50;
