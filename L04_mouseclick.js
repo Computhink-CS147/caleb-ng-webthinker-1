12dfa9eeb455adfb1a1f92d11a3a8c0dd1a3ab6e
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-26 03:27:50 +0000

    Jul 26, 2026, 3:27 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,7 +4,23 @@ function setup() {
     createCanvas(600, 800);
     background("aqua");
 }
-
+function setup() {
+    createCanvas(600, 400);
+    background(220);
+    noLoop(); // Stops continuous drawing
+  }
+  
 function draw() {
-    triangle(20,80,60,30,70,80);
+  circle(75,200,100);
+  circle(225,200,100);
+  circle(375,200,100);
+  circle(525,200,100);
+  // Recap 1: Repeating Circles
+let circleDiameter= 50;
+let circleY=50;
+for(let num=1; num<9; num++) {
+  fill(num*50,150,100)
+  circle(50*num,circleY*num,circleDiameter);
 }
+}
+print(width);
