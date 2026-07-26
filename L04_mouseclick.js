fbebbc0a7540945bc9c2682c572c359fee3e55a3
Computhink-CS147 <cs147@computhink.com.sg>
2026-07-26 03:54:21 +0000

    Jul 26, 2026, 3:54 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -12,16 +12,4 @@ function setup() {
   
 function draw() {
   circle(75,200,100);
-  circle(225,200,100);
-  circle(375,200,100);
-  circle(525,200,100);
-  // Recap 1: Repeating Circles
-let circleDiameter= 50;
-let circleY=50;
-let circleX=350;
-for(let num=1; num<6; num++) {
-  fill(num*50,150,100)
-  circle(50*num,circleX*num,circleDiameter);
 }
-}
-print(width);
