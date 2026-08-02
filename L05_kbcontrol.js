fc4c7ab50669ac6de4ddaf172137bcb12d03ce14
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-02 03:53:44 +0000

    Aug 2, 2026, 3:53 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,7 +2,11 @@ let size=250
 let colour=blue
 let showCircle=false
 let showRect=false
-let showTri=fals
+let showTri=false
+function setup () {
+    createCanvas(600,600);
+   background=("skyblue");
+}
 
 
 function draw() {
