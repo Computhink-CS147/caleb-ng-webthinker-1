b0d4454391a0ffdfdb3919ae27825e7463853ad1
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-02 03:38:58 +0000

    Aug 2, 2026, 3:38 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,16 +1,18 @@
-// write your codes here
+let size=5;
+let colour="blue";
 
 function setup() {
-    createCanvas(600, 800);
-    background("aqua");
+    createCanvas(2000,2000);
+    background("skyblue");
+    noStroke();
 }
-  
-let shapeColor="aqamarime"
-Fill(shapeColor);
-nostroke();
-function mousePressed (){
-    shapeColor="white"
+
+function mousePressed() {
+    size=5;
+    colour=color(random(0,255), random(0,255),random(0,255));
 }
-function mouseDragged (){
-    circle(mouseX, mouseY, 20);
+function mouseDragged() {
+    fill(colour);
+    circle(mouseX, mouseY, size);
+    size=size +0.5;
 }
