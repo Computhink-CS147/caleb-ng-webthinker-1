cc72e7b41cc00f5b419946fe9bdd46862d68a274
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-30 03:52:00 +0000

    Aug 30, 2026, 3:52 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -27,9 +27,9 @@ function draw() {
        text("HE SHOWED ME HIS SUPER BIG DING A LING",75,ypos+150);
        text("JEFFERY EPSTEIN TOUCHED ME WHEN I WAS A TEEN",75,ypos+170);
        text("WHEN I MET MY UNCLE S**T GOT FREAKY",75,ypos+190);
-       text("TURNS OUT ME N HIM ARE MAD ZESTY",75,ypos+110);
-       text("9/11 OSAMA IS RELATED TO OBAMA",75,ypos+110);
-       text("HIT**R DEFINETLY HAD A MICROP***S",75,ypos+110);
+       text("TURNS OUT ME N HIM ARE MAD ZESTY",75,ypos+210);
+       text("9/11 OSAMA IS RELATED TO OBAMA",75,ypos+230);
+       text("HIT**R DEFINETLY HAD A MICROP***S",75,ypos+20);
        text("TO EXCITE MYSELF IS SHOVE SOME HOT WHEELS UP MY A**S",75,ypos+110);
        text("C**SHOT SO POWERFUL IT REACHED VENUS",75,ypos+110);
        text("KAI CENAT IS A PERFORMATIVE DINGUUS",75,ypos+110);
