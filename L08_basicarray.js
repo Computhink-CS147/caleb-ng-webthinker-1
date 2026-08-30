e2220468a7a73bf8ff519c3222fda185807e57d1
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-30 03:48:29 +0000

    Aug 30, 2026, 3:48 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -25,7 +25,9 @@ function draw() {
        text("KIM JONG UN IS MASTER OF GOON",75,ypos+130);
        text("ME N P DIDDY HAVE A SNEAKY SNEAKY LINK",75,ypos+110);
        text("HE SHOWED ME HIS SUPER BIG DING A LING",75,ypos+110);
+       text("JEFFERY EPSTEIN TOUCHED ME WHEN I WAS A TEEN",75,ypos+110);
        text("IM WEIRD AS",75,ypos+110);
+       
 
        
 
