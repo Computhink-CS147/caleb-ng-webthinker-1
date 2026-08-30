46224c59ea447f404875bd469648d655f3778aa1
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-30 03:46:46 +0000

    Aug 30, 2026, 3:46 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -22,5 +22,7 @@ function draw() {
      text("TOLD BRO I HAVE CP BECAUSE I WAS MENTAL",75,ypos+70);
       text("MY BREATH SMELLS LIKE S**T I NEED A MENTO",75,ypos+90);
        text("IM WEIRD AS S**T I JUST LICKED MY TOE",75,ypos+110);
+       text("IM WEIRD AS",75,ypos+110);
+
     ypos -= 0.67;
 }
