dc7c683e32f0b89e53452c740f3f4b0e6dd71bae
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-30 03:35:47 +0000

    Aug 30, 2026, 3:35 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -17,6 +17,6 @@ function draw() {
     textSize(18);
     text("Hyperbaiter",135,ypos);
      text("♪♪♪",165,ypos+30);
-     text("THE ROCK ON MY A** MADE ME DISABLED")
+     text("THE ROCK ON MY A** MADE ME DISABLED",135)
     ypos -= 0.67;
 }
