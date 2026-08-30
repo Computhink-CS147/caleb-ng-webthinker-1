fcc45be7c4e626cd142c23b0338c6999ac8a703c
Computhink-CS147 <cs147@computhink.com.sg>
2026-08-30 03:46:17 +0000

    Aug 30, 2026, 3:46 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -21,6 +21,6 @@ function draw() {
      text("THE ROCK ON MY A** MADE ME DISABLED",75,ypos+50);
      text("TOLD BRO I HAVE CP BECAUSE I WAS MENTAL",75,ypos+70);
       text("MY BREATH SMELLS LIKE S**T I NEED A MENTO",75,ypos+90);
-       text("IM",75,ypos+110);
+       text("IM WEIRD AS ",75,ypos+110);
     ypos -= 0.67;
 }
