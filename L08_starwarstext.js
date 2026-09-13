0f08458afb8a638e3199f9fac5f4c196a127af44
Computhink-CS147 <cs147@computhink.com.sg>
2026-09-13 03:47:47 +0000

    Sep 13, 2026, 3:47 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,23 +1,30 @@
-// write your codes here
-let lineGap=60;
-let yPos=400;
-function setup(){
-    createCanvas(400,400);
-}
-function draw(){
-    // background(220);
-    // textSize(20);
-    textSize(20);
-    textAlign(LEFT);
-    text("My Favourite Foods");
-    for(let i=0; i<FileList,length;i++);{
-        text(list[1],50*50)
-        yPos-= 0.6;
-        if(ypos< -list.length*lineGap)
-            if(yPos<list.length*lineGap){
+// // write your codes here
+// let lineGap=60;
+// let yPos=400;
+// function setup(){
+//     createCanvas(400,400);
+// }
+// function draw(){
+//     // background(220);
+//     // textSize(20);
+//     textSize(20);
+//     textAlign(LEFT);
+//     text("My Favourite Foods");
+//     for(let i=0; i<FileList,length;i++);{
+//         text(list[1],50*50)
+//         yPos-= 0.6;
+//         if(ypos< -list.length*lineGap)
+//             if(yPos<list.length*lineGap){
 
-            }
-            yPos=height;
+//             }
+//             yPos=height;
 
-    }
-}
+//     }
+// }
+
+let storyText=[
+    "A long time ago in a galaxy far",
+    "far away",
+    "it is a gooner",
+    
+]
