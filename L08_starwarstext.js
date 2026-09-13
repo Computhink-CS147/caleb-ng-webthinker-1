5336f93dd29aea05aab0b367aa63ef782fe5a009
Computhink-CS147 <cs147@computhink.com.sg>
2026-09-13 04:09:20 +0000

    Sep 13, 2026, 4:09 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -21,5 +21,61 @@
 
 //     }
 // }
+let ypos =0;
+let lineY=0;
+let storyText=[];
 
-    
+function setup() {
+    createCanvas(400,600);
+    background(220);
+    ypos= height;
+    storyText[0]="goon goon goon"
+    storyText[1]="6767"
+    storyText[2]="lick lick lick"
+    print(storyText);
+}
+
+function draw() {
+    background(220);
+    textSize(18);
+    text("Hyperbaiter",135,ypos);
+     text("♪♪♪",165,ypos+30);
+     textSize(10);
+     text("THE ROCK ON MY A** MADE ME DISABLED",75,ypos+50);
+     text("TOLD BRO I HAVE CP BECAUSE I WAS MENTAL",75,ypos+70);
+      text("MY BREATH SMELLS LIKE S**T I NEED A MENTO",75,ypos+90);
+       text("IM WEIRD AS S**T I JUST LICKED MY TOE",75,ypos+110);
+       text("KIM JONG UN IS MASTER OF GOON",75,ypos+130);
+       text("ME N P DIDDY HAVE A SNEAKY SNEAKY LINK",75,ypos+160);
+       text("HE SHOWED ME HIS SUPER BIG DING A LING",75,ypos+190);
+       text("JEFFERY EPSTEIN TOUCHED ME WHEN I WAS A TEEN",75,ypos+210);
+       text("WHEN I MET MY UNCLE S**T GOT FREAKY",75,ypos+230);
+       text("TURNS OUT ME N HIM ARE MAD ZESTY",75,ypos+260);
+       text("9/11 OSAMA IS RELATED TO OBAMA",75,ypos+290);
+       text("HIT**R DEFINETLY HAD A MICROP***S",75,ypos+310);
+       text("TO EXCITE MYSELF IS SHOVE SOME HOT WHEELS UP MY A**S",75,ypos+330);
+       text("C**SHOT SO POWERFUL IT REACHED VENUS",75,ypos+360);
+       text("KAI CENAT IS A PERFORMATIVE DINGUUS",75,ypos+390);
+       text("♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪",75,ypos+500);
+        text("WHAT HAPPENED IT IS THE NEW WAVE",75,ypos+520);
+         text("THEN HE LOST ALL HIS MONEY THE VERY NEXT DAY",75,ypos+540);
+          text("THEN I LOST 5K ON A POKER PLAY",75,ypos+560);
+           text("THEN MY CRUSH TURNED DOWN MY DATE",75,ypos+580);
+           text("NOW ITS TIME TO TURN GAY",75,ypos+600);
+            t
+            ext("N BE A FEMBOY ON ONLYFANSSSSSS",75,ypos+620);
+
+         text("♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪",75,ypos+700);
+          text("THE END",75,ypos+800);
+
+
+       
+
+
+       
+       
+
+       
+
+    ypos -=  0.2;
+}
