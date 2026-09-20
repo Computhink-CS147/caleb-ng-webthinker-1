722fba9fe6c7a9a1582cda152b471cacbbd3ae78
Computhink-CS147 <cs147@computhink.com.sg>
2026-09-20 03:41:52 +0000

    Sep 20, 2026, 3:41 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,18 +1,18 @@
 ///ssssss
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    textAlign(CENTER, CENTER);
-}
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     textAlign(CENTER, CENTER);
+// }
 
 
-function draw(){
-background(220);
-let h = hour();
-let m = minute();
-let s = second();
-fill(0);
-textSize(72);
-text(nf(h, 2) + ":" + nf(m, 2) + ":" + nf(s, 2), width/2, height/2);
+// function draw(){
+// background(220);
+// let h = hour();
+// let m = minute();
+// let s = second();
+// fill(0);
+// textSize(72);
+// text(nf(h, 2) + ":" + nf(m, 2) + ":" + nf(s, 2), width/2, height/2);
 
-}
+// }
